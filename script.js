a66d1ac5c6a3df236c132f73b8b6ac56d7cce7ab
Gabriel Marques <gabriel.marq94@gmail.com>
2022-08-09 14:49:52 -0300

    Atribuindo Funções
    Atribuindo funções a cada uma das páginas, fazendo as conversões de acordo com cada formato de medida. Começando pelos conversores de temperatura

--- script.js
+++ script.js
@@ -9,3 +9,21 @@ botao.forEach(box => {
     })
 })
 
+function calcCelsius() {
+var cel = document.getElementById('numcel')
+cel = parseFloat(cel.value).toFixed(2).replace(",", ".")
+var rescel = document.querySelector('div#rescel')
+var fah = cel * 1.8 + 32
+var kel = cel + 273
+
+rescel.innerHTML = `${cel.replace('.', ',')} ºC equivalem a ${fah.toFixed(2).replace('.', ',')} ºF e ${fah.toFixed(2).replace('.', ',')}  K.`
+}
+
+function calcKelvin() {
+    var kel = document.querySelector('input#numkelvin')
+    kel = parseFloat(kel.value).toFixed(2).replace(',', '.')
+    var reskel = document.querySelector('div#reskel')
+    var cel = kel - 273
+    var fah = 1.8 * (cel) + 32
+    reskel.innerHTML = `${kel.replace('.', ',')} K equivalem a <strong>${cel.toFixed(2).replace('.', ',')} ºC</strong> e <strong>${fah.toFixed(2).replace('.' , ',')} ºF.</strong>`   
+}
